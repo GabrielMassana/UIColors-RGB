5fe11a00b41d80e4e6f5b8d1ff9abe79bc353143
Gabriel Massana <gabriel.massana@irisvalet.com>
2013-11-13 10:39:29 +0000

    first commit

--- colors/ViewController.h
+++ colors/ViewController.h
@@ -10,4 +10,37 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIButton *btn1;
+@property (strong, nonatomic) IBOutlet UIButton *btn2;
+@property (strong, nonatomic) IBOutlet UIButton *btn3;
+@property (strong, nonatomic) IBOutlet UIButton *btn4;
+@property (strong, nonatomic) IBOutlet UIButton *btn5;
+@property (strong, nonatomic) IBOutlet UIButton *btn6;
+@property (strong, nonatomic) IBOutlet UIButton *btn7;
+@property (strong, nonatomic) IBOutlet UIButton *btn8;
+@property (strong, nonatomic) IBOutlet UIButton *btn9;
+@property (strong, nonatomic) IBOutlet UIButton *btn10;
+@property (strong, nonatomic) IBOutlet UIButton *btn11;
+@property (strong, nonatomic) IBOutlet UIButton *btn12;
+@property (strong, nonatomic) IBOutlet UIButton *btn13;
+@property (strong, nonatomic) IBOutlet UIButton *btn14;
+@property (strong, nonatomic) IBOutlet UIButton *btn15;
+
+
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
+@property (strong, nonatomic) IBOutlet UILabel *label3;
+@property (strong, nonatomic) IBOutlet UILabel *label4;
+@property (strong, nonatomic) IBOutlet UILabel *label5;
+@property (strong, nonatomic) IBOutlet UILabel *label6;
+@property (strong, nonatomic) IBOutlet UILabel *label7;
+@property (strong, nonatomic) IBOutlet UILabel *label8;
+@property (strong, nonatomic) IBOutlet UILabel *label9;
+@property (strong, nonatomic) IBOutlet UILabel *label10;
+@property (strong, nonatomic) IBOutlet UILabel *label11;
+@property (strong, nonatomic) IBOutlet UILabel *label12;
+@property (strong, nonatomic) IBOutlet UILabel *label13;
+@property (strong, nonatomic) IBOutlet UILabel *label14;
+@property (strong, nonatomic) IBOutlet UILabel *label15;
+
 @end
--- colors/ViewController.m
+++ colors/ViewController.m
@@ -17,7 +17,60 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self.btn1 setBackgroundColor:[UIColor blackColor]];
+    [self.btn2 setBackgroundColor:[UIColor darkGrayColor]];
+    [self.btn3 setBackgroundColor:[UIColor lightGrayColor]];
+    [self.btn4 setBackgroundColor:[UIColor whiteColor]];
+    [self.btn5 setBackgroundColor:[UIColor grayColor]];
+    [self.btn6 setBackgroundColor:[UIColor redColor]];
+    [self.btn7 setBackgroundColor:[UIColor greenColor]];
+    [self.btn8 setBackgroundColor:[UIColor blueColor]];
+    [self.btn9 setBackgroundColor:[UIColor cyanColor]];
+    [self.btn10 setBackgroundColor:[UIColor yellowColor]];
+    [self.btn11 setBackgroundColor:[UIColor magentaColor]];
+    [self.btn12 setBackgroundColor:[UIColor orangeColor]];
+    [self.btn13 setBackgroundColor:[UIColor purpleColor]];
+    [self.btn14 setBackgroundColor:[UIColor brownColor]];
+    [self.btn15 setBackgroundColor:[UIColor clearColor]];
+    
+    
+    [self.btn1 setTitle:@"blackColor" forState:UIControlStateNormal];
+    [self.btn2 setTitle:@"darkGrayColor" forState:UIControlStateNormal];
+    [self.btn3 setTitle:@"lightGrayColor" forState:UIControlStateNormal];
+    [self.btn4 setTitle:@"whiteColor" forState:UIControlStateNormal];
+    [self.btn5 setTitle:@"grayColor" forState:UIControlStateNormal];
+    [self.btn6 setTitle:@"redColor" forState:UIControlStateNormal];
+    [self.btn7 setTitle:@"greenColor" forState:UIControlStateNormal];
+    [self.btn8 setTitle:@"blueColor" forState:UIControlStateNormal];
+    [self.btn9 setTitle:@"cyanColor" forState:UIControlStateNormal];
+    [self.btn10 setTitle:@"yellowColor" forState:UIControlStateNormal];
+    [self.btn11 setTitle:@"magentaColor" forState:UIControlStateNormal];
+    [self.btn12 setTitle:@"orangeColor" forState:UIControlStateNormal];
+    [self.btn13 setTitle:@"purpleColor" forState:UIControlStateNormal];
+    [self.btn14 setTitle:@"brownColor" forState:UIControlStateNormal];
+    [self.btn15 setTitle:@"clearColor" forState:UIControlStateNormal];
+    
+    
+    [self.label1 setText:@"R:0 G:0 B:0"];
+    [self.label2 setText:@"R:85 G:85 B:85"];
+    [self.label3 setText:@"R:170 G:170 B:170"];
+    [self.label4 setText:@"R:255 G:255 B:255"];
+    [self.label5 setText:@"R:128 G:128 B:128"];
+    [self.label6 setText:@"R:255 G:0 B:0"];
+    [self.label7 setText:@"R:0 G:255 B:0"];
+    [self.label8 setText:@"R:0 G:0 B:255"];
+    [self.label9 setText:@"R:0 G:255 B:255"];
+    [self.label10 setText:@"R:255 G:255 B:0"];
+    [self.label11 setText:@"R:255 G:0 B:255"];
+    [self.label12 setText:@"R:255 G:128 B:0"];
+    [self.label13 setText:@"R:128 G:0 B:128"];
+    [self.label14 setText:@"R:153 G:102 B:51"];
+    [self.label15 setText:@"Alpha:0"];
+    
+
+
+
 }
 
 - (void)didReceiveMemoryWarning
