7c85b8f4ff005925302b75869528e39dfabe1789
Gabriel Massana <gabriel.massana@irisvalet.com>
2013-11-13 09:55:32 +0000

    Initial Commit

---
+++ colors/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  colors
+//
+//  Created by Gabriel Massana on 11/13/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ colors/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  colors
+//
+//  Created by Gabriel Massana on 11/13/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
